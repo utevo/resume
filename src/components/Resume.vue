bb6f437040d0beba705a0c79d305d6d54aaca2fa
Michał Kowieski <kowieski.m98@gmail.com>
2020-04-05 18:41:07 +0200

    Repair Resume templete

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="resume">
     <div class="left-column">
-
       <div class="headline">
         <div class="headline-name">
           <span>{{ person.name.first }} {{ person.name.middle }}</span>
@@ -18,61 +17,51 @@
       <div class="txt-multiline">{{ person.about }}</div>
 
       <div class="txt-multiline">{{ person.knowledge }}</div>
-    </div>
 
-    <div class="contact-container">
-      <a v-if="person.contact.email"
-        :href="contactLinks.email"
-        class="external-link">
-        <i class="contact-icon material-icons">mail</i>
-        <span class="block-marged txt-full-white">
-          {{ person.contact.email }}
-        </span>
-      </a>
+      <div class="contact-container">
+        <a v-if="person.contact.email" :href="contactLinks.email" class="external-link">
+          <i class="contact-icon material-icons">mail</i>
+          <span class="block-marged txt-full-white">{{ person.contact.email }}</span>
+        </a>
 
-      <a v-if="person.contact.phone"
-        :href="contactLinks.phone"
-        class="external-link">
-        <i class="contact-icon material-icon">mail</i>
-        <span class="block-marged txt-full-white">
-          {{ person.contact.phone }}
-        </span>
-      </a>
+        <a v-if="person.contact.phone" :href="contactLinks.phone" class="external-link">
+          <i class="contact-icon material-icon">mail</i>
+          <span class="block-marged txt-full-white">{{ person.contact.phone }}</span>
+        </a>
 
-      <a v-if="person.contact.github"
-        :href="contactLinks.github"
-        class="external-link">
-        <i class="contact-icon fa fa-github"></i>
-        <span class="block-marged txt-full-white">
-          {{ person.contact.github }}
-        </span>
-      </a>
-    </div>
+        <a v-if="person.contact.github" :href="contactLinks.github" class="external-link">
+          <i class="contact-icon fa fa-github"></i>
+          <span class="block-marged txt-full-white">{{ person.contact.github }}</span>
+        </a>
+      </div>
 
-    <div class="skills-container">
-      <span class="subheadline">Skills</span>
-      <a v-for="(skill, index) in person.skills" :key="index"
-        class="skill-item"
-        :href="skill.url">
-        <i v-if="skill.iconClass" :class="'skill-icon' + skill.iconClass"></i>
-        <i v-if="skill.materialIcon" class="skill-icon material-icon"> {{ skill.materialIcon }} </i>
-        <span class="block-marged txt-full-white">
-          {{ skill.name }}
-        </span>
-      </a>
-    </div>
+      <div class="skills-container">
+        <span class="subheadline">Skills</span>
+        <a
+          v-for="(skill, index) in person.skills"
+          :key="index"
+          class="skill-item"
+          :href="skill.url"
+        >
+          <i v-if="skill.iconClass" :class="'skill-icon' + skill.iconClass"></i>
+          <i v-if="skill.materialIcon" class="skill-icon material-icon">{{ skill.materialIcon }}</i>
+          <span class="block-marged txt-full-white">{{ skill.name }}</span>
+        </a>
+      </div>
 
-    <div class="hobbies-container">
-      <span class="subheadline">Hobbies</span>
-      <a v-for="(hobby, index) in person.hobbies" :key="index"
-        class="hobby-item"
-        :href="hobby.url">
-        <i v-if="hobby.iconClass" :class="'hobby-icon' + hobby.iconClass"></i>
-        <i v-if="hobby.materialIcon" class="hobby-icon material-icon"> {{ hobby.materialIcon }} </i>
-        <span class="block-marged txt-full-white">
-          {{ hobby.name }}
-        </span>
-      </a>
+      <div class="hobbies-container">
+        <span class="subheadline">Hobbies</span>
+        <a
+          v-for="(hobby, index) in person.hobbies"
+          :key="index"
+          class="hobby-item"
+          :href="hobby.url"
+        >
+          <i v-if="hobby.iconClass" :class="'hobby-icon' + hobby.iconClass"></i>
+          <i v-if="hobby.materialIcon" class="hobby-icon material-icon">{{ hobby.materialIcon }}</i>
+          <span class="block-marged txt-full-white">{{ hobby.name }}</span>
+        </a>
+      </div>
     </div>
   </div>
 </template>
