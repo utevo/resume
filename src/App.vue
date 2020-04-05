1fa4292d02d6b9a2cb076e60851b41ffff6a7d20
Michał Kowieski <kowieski.m98@gmail.com>
2020-04-05 14:02:55 +0200

    Init Resume component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Resume msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Resume from './components/Resume.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Resume
   }
 }
 </script>
